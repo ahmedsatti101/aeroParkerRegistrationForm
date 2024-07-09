0cd450c50f5d3573c3edd502aae5a460c2da9fe2
Ahmed Mohamed <ahmedysatti@gmail.com>
2024-07-09 20:00:10 +0100

    Created database

---
+++ src/main/java/com/aeroParker/registrationForm/Customers/Customers.java
@@ -0,0 +1,167 @@
+package com.aeroParker.registrationForm.Customers;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customers")
+public class Customers {
+    @Column
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "registered", nullable = false)
+    private LocalDateTime registered;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Override
+    public String toString() {
+        return "Customers{" +
+                "id=" + id +
+                ", registered=" + registered +
+                ", email='" + email + '\'' +
+                ", title='" + title + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", addressLineOne='" + addressLineOne + '\'' +
+                ", addressLineTwo='" + addressLineTwo + '\'' +
+                ", city='" + city + '\'' +
+                ", postcode='" + postcode + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "address_line_1", nullable = false)
+    private String addressLineOne;
+
+    @Column(name = "address_line_2")
+    private String addressLineTwo;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "postcode", nullable = false)
+    private String postcode;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public Customers() {
+
+    }
+
+    public Customers(int id, LocalDateTime registered, String email, String title, String firstName, String lastName, String addressLineOne, String addressLineTwo, String city, String postcode, String phoneNumber) {
+        this.id = id;
+        this.registered = registered;
+        this.email = email;
+        this.title = title;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.addressLineOne = addressLineOne;
+        this.addressLineTwo = addressLineTwo;
+        this.city = city;
+        this.postcode = postcode;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(LocalDateTime registered) {
+        this.registered = registered;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddressLineOne() {
+        return addressLineOne;
+    }
+
+    public void setAddressLineOne(String addressLineOne) {
+        this.addressLineOne = addressLineOne;
+    }
+
+    public String getAddressLineTwo() {
+        return addressLineTwo;
+    }
+
+    public void setAddressLineTwo(String addressLineTwo) {
+        this.addressLineTwo = addressLineTwo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/com/aeroParker/registrationForm/Customers/CustomersRepository.java
@@ -0,0 +1,6 @@
+package com.aeroParker.registrationForm.Customers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomersRepository extends JpaRepository<Customers, Integer> {
+}
